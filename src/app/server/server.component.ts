4b19e04c46178f6efafbb681366b487459ddbcf3
Piyush Mehta <me@piyushmehta.com>
2019-06-20 00:27:01 +0530

    string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,4 +3,7 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 10;
+  serversStatus: string = 'offline';
+}

ff2ac88c8e5e6b99234d879c04b0a3890ccdd70a
Piyush Mehta <me@piyushmehta.com>
2019-06-20 00:42:50 +0530

    dynamic content

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
+  allowNewServer = false;
   constructor() { }
 
   ngOnInit() {
